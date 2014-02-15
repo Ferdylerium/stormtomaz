e69cef3f48e52307667303dbf57d6beb826bbb88
Ferdinand Ribbink <stormtomaz@outlook.com>
2014-02-15 03:11:43 -0500

    bug

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-var express = require("express"),
-    path = require("path");
-    
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var routes = require('./routes');
+
 var app = express();
 var port = Number(process.env.PORT || 5000);
 
-app.configure(function(){
+app.configure(function(){   
     app.set('view engine', 'jade');
     app.set('views', __dirname + '/views');
+    app.use(express.favicon());
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/', function(req,res){
-   res.render('index'); 
+
+app.get('/', routes.index);
+
+/*app.get('/', function(req,res){
+   res.render('home'); 
 });
 
 app.get('/:viewname', function(req,res){
    res.render(req.params.viewname, { layout: '_root'}); 
-});
+});*/
 
 app.listen(port);
